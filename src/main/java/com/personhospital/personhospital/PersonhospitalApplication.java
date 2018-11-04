a40174f8e58ce7247c1f47ca9b6488b358f78071
NavyGunti <navyagunti3@gmail.com>
2018-11-04 11:16:34 +0530

    haii

---
+++ src/main/java/com/personhospital/personhospital/PersonhospitalApplication.java
@@ -0,0 +1,12 @@
+package com.personhospital.personhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonhospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonhospitalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/personhospital/personhospital/PersonhospitalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.personhospital.personhospital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonhospitalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
